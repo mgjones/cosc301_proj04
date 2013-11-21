ad7b8a2a5232b236685592d341ab9c6197ce937a
nolango <nolangonzalez@live.com>
2013-11-21 03:04:21 -0500

    updated main.c
    Still lots of bugs but fixed it to the best of our abilities

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-/*
- * Nolan Gonzalez and Mariah Jones
- * November 20, 2013
-*/
-
+//
+//  By Mariah Jones &
+//  Nolan Gonzalez
+//
+//
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,14 +20,17 @@
 
 struct work_queue_item{
 	int sock;
+	// for weblog purposes
+	struct sockaddr_in client_address;
 	struct work_queue_item *next;
 };
 
 
 // globals //
-struct queue *head = NULL;
-struct queue *tail = NULL;
+struct work_queue_item *head = NULL;
+struct work_queue_item *tail = NULL;
 pthread_mutex_t workmutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t loglock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t workcond = PTHREAD_COND_INITIALIZER;
 int count = 0;
 
@@ -50,31 +53,106 @@ void usage(const char *progname) {
 
 //	worker and queue functions	//
 
-void *worker(void* arg){
-	pthread_mutex_lock(&workmutex);
-	while(count == 0){
-		pthread_cond_wait(&workcond, &workmutex);
+void additem(int sock, struct sockaddr_in client_address){
+	struct work_queue_item *new_item = malloc(sizeof(struct work_queue_item));
+	new_item->client_address = client_address;
+	new_item->sock = sock;
+
+	if (head == NULL){ // no nodes
+		new_item->next = NULL;
+		head = new_item; 
+		tail = head;
+	}else{ // some nodes
+		new_item->next = head;
+		head = new_item;
 	}
-	int sock = removeitem();	
-	pthread_mutex_unlock(&workmutex);
-
-	// respond to request //
-	char *reqbuffer = NULL;
-	int x = getrequest(sock, reqbuffer,1024);
-	if(x == -1)
-		printf("Error: could not get request\n");
-	// check using stat //
-	//	  senddata		//
-	
-	
-	return NULL;
-}
-
-void add(int sock){
 	return;
 }
 
+int removeitem(){
+	struct work_queue_item *traverse = head;
+	int i = 0;
+	int retsock;
+	if (head == NULL){
+		return 0;
+	}else if (head->next == NULL){ // only 1 node
+		retsock = tail->sock;
+		free(tail);
+		// being cautious because
+		// they're global
+		tail = NULL;
+		head = NULL;
+		count--;
+		return retsock;
+	}else{
+		for (; i < count - 1; i++){
+			traverse = traverse->next;
+		}
+		// tail points to node before last node
+		tail = traverse;
+		int retsock = (traverse + 1)->sock;
+		free(traverse+1);
+		traverse->next = NULL;
+		count--;
+		return retsock;
+	}
+	return 0;
+}
+
+
+void *worker(void* arg){
+	while(1){
+		pthread_mutex_lock(&workmutex);
+		while(count == 0){
+			pthread_cond_wait(&workcond, &workmutex);
+		}
+		struct sockaddr_in client_address = tail->client_address;
+		int sock = removeitem();	
+		pthread_mutex_unlock(&workmutex);
+
+		// respond to request //
+		char reqbuffer[1024];
+		int x = getrequest(sock, reqbuffer,1024);
+		struct stat buffer;
+		if(x == -1){
+			printf("Error: could not get request\n");
+			return NULL;
+		}
+
+		int err = stat(reqbuffer, &buffer);
+		if(err == -1){
+			printf("file %s doesn't exist\n",reqbuffer);
+			senddata(sock, HTTP_404, sizeof(HTTP_404));
+			return NULL;
+		}
+		senddata(sock, HTTP_200, err);	
+
+		int file = open(reqbuffer, O_RDONLY); //S_IRUSR - no workie
+		int size;
+		while(1){
+			// sending some arbitrary size, 1024,
+			// because it probably doesn't matter in
+			// how many increments it reads in the file
+			size = read(file, &buffer, 1024);
+			if(size == 0)
+				break;
+			senddata(sock, HTTP_200, size);
+		}
+
+		// printing to log //
+		pthread_mutex_lock(&loglock);
+		FILE *weblog = fopen("weblog.txt", "w");
+		time_t now = time(NULL);
+		fprintf(weblog, "Got connection from %s:%d at %s\n", inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port), ctime(&now));
+		pthread_mutex_unlock(&loglock);
+
+		close(file);
+	}
+	return NULL;
+}
+
 //	//	//	//	//	//	//	//	//
+
 
 void runserver(int numthreads, unsigned short serverport) {
     //////////////////////////////////////////////////
@@ -88,7 +166,7 @@ void runserver(int numthreads, unsigned short serverport) {
 	while(i < numthreads){
 		pthread_create(&threads[i],NULL,worker,NULL);
 		i++;
-	}
+	}	
 	
     //////////////////////////////////////////////////
     int main_socket = prepare_server_socket(serverport);
@@ -134,7 +212,7 @@ void runserver(int numthreads, unsigned short serverport) {
 
 
 			pthread_mutex_lock(&workmutex);
-			add(new_sock);
+			additem(new_sock,client_address);
 			count++;
 			pthread_cond_signal(&workcond);
 			pthread_mutex_unlock(&workmutex);
